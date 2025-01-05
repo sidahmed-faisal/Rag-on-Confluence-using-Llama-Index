ad6759da60a34982f3ef709ab97d0ec72e5f3c6d
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2025-01-05 20:07:13 +0400

    Update api_utils.py

--- app/api_utils.py
+++ app/api_utils.py
@@ -25,3 +25,38 @@ def list_pages(spaceKey):
     except Exception as e:
         st.error(f"An error occurred while fetching the document list: {str(e)}")
         return []
+
+def get_chat_answer(space, page, question):
+            
+    url = "http://localhost:8000/chat"
+    
+    headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    
+    if page == None:
+
+        data = {
+            "question": question,
+            "space": space
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        return response.json()
+    
+    else:
+        
+        data = {
+            "question": question,
+            "space": space,
+            "page_id": [
+                "294939"
+            ]
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        return response.json()
+
